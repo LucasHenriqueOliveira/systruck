961b0a952e1559048a814d5e839007614cd09dbc
Lucas <lucashen@gmail.com>
2015-12-21 09:36:16 -0200

    add truck controller

---
+++ js/controllers/truck.controller.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('TruckController', TruckController);
+
+    TruckController.$inject = ['$location'];
+
+    function TruckController($location) {
+        var vm = this;
+
+    }
+
+})();
+
