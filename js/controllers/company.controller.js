6456ed28ee98d6b1d3845480561ec28cbbe2adb1
Lucas <lucashen@gmail.com>
2016-01-03 09:17:33 -0200

    add company controller

---
+++ js/controllers/company.controller.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CompanyController', CompanyController);
+
+    CompanyController.$inject = ['$location'];
+
+    function CompanyController($location) {
+        var vm = this;
+
+    }
+
+})();
+
+
