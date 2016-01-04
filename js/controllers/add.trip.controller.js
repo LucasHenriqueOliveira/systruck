d92f661c21b0886207c949a131e7f91efe5150d7
Lucas <lucashen@gmail.com>
2016-01-04 08:51:07 -0200

    add controller add trip

---
+++ js/controllers/add.trip.controller.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AddTripController', AddTripController);
+
+    AddTripController.$inject = ['$location'];
+
+    function AddTripController($location) {
+        var vm = this;
+
+    }
+
+})();
+
