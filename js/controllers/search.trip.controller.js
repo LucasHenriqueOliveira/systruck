4aa70961f75bf15c826601499a1ea8648e80ef29
Lucas <lucashen@gmail.com>
2016-01-04 08:59:23 -0200

    add controller search trip

---
+++ js/controllers/search.trip.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('SearchTripController', SearchTripController);
+
+    SearchTripController.$inject = ['$location'];
+
+    function SearchTripController($location) {
+        var vm = this;
+
+    }
+
+})();
