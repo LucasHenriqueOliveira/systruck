5d5b6081d966bb3bc96db36ed85ccf7314c14b67
Lucas <lucashen@gmail.com>
2016-01-10 10:50:44 -0200

    add controller search maintenance

---
+++ js/controllers/search.maintenance.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('SearchMaintenanceController', SearchMaintenanceController);
+
+    SearchMaintenanceController.$inject = ['$location'];
+
+    function SearchMaintenanceController($location) {
+        var vm = this;
+
+    }
+
+})();
