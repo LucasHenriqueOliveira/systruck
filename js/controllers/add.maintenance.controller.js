dea1c3742b05e93098b458dec65ba556e09eebd9
Lucas <lucashen@gmail.com>
2016-01-10 10:23:30 -0200

    add controller add maintenance

---
+++ js/controllers/add.maintenance.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AddMaintenanceController', AddMaintenanceController);
+
+    AddMaintenanceController.$inject = ['$location'];
+
+    function AddMaintenanceController($location) {
+        var vm = this;
+
+    }
+
+})();
