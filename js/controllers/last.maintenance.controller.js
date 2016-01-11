af82304801d01c10a9d4a07a8c3690e93ee1d250
Lucas <lucashen@gmail.com>
2016-01-11 11:35:53 -0200

    add controller last maintenance

---
+++ js/controllers/last.maintenance.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LastMaintenanceController', LastMaintenanceController);
+
+    LastMaintenanceController.$inject = ['$location'];
+
+    function LastMaintenanceController($location) {
+        var vm = this;
+
+    }
+
+})();
