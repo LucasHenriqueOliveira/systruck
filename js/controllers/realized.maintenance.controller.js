cd69078bb7407a597583340f8822889bd940d0df
Lucas <lucashen@gmail.com>
2016-01-14 14:22:10 -0200

    add controller realized maintenance

---
+++ js/controllers/realized.maintenance.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RealizedMaintenanceController', RealizedMaintenanceController);
+
+    RealizedMaintenanceController.$inject = ['$location'];
+
+    function RealizedMaintenanceController($location) {
+        var vm = this;
+
+    }
+
+})();
