d4ec8039c00389dd4c680633a067b9c43a2ab459
Lucas <lucashen@gmail.com>
2016-02-05 22:05:30 -0200

    change size height to step

--- js/controllers/add.maintenance.controller.js
+++ js/controllers/add.maintenance.controller.js
@@ -5,14 +5,18 @@
         .module('app')
         .controller('AddMaintenanceController', AddMaintenanceController);
 
-    AddMaintenanceController.$inject = ['$location', '$localstorage'];
+    AddMaintenanceController.$inject = ['$location', '$localstorage', 'DataService'];
 
-    function AddMaintenanceController($location, $localstorage) {
+    function AddMaintenanceController($location, $localstorage, DataService) {
         var vm = this;
         vm.message = '';
         vm.qtd_km = '';
-        vm.message_price = '';
-        vm.part_price = '';
+        vm.part = {};
+        vm.part.message_price = '';
+        vm.part.price = '';
+        vm.view_status = false;
+        vm.view_date = false;
+        vm.parts = DataService.getPart();
 
         vm.trucks = [
             {
@@ -58,45 +62,55 @@
             }
         ];
 
-        vm.parts = [
+        vm.parts_maintenance = [
             {
                 id: 1,
+                name: 'Óleo',
                 price: "1200"
             },
             {
                 id: 2,
+                name: 'Pneu',
                 price: "2400"
             },
             {
                 id: 3,
+                name: 'Suspensão',
                 price: "1350"
             },
             {
                 id: 4,
+                name: 'Feixe de molas',
                 price: "3000"
             },
             {
                 id: 5,
+                name: 'Amortecedores',
                 price: "4350"
             },
             {
                 id: 6,
+                name: 'Sistema de Freio',
                 price: "1570"
             },
             {
                 id: 7,
+                name: 'Molas Pneumáticas',
                 price: "1240"
             },
             {
                 id: 8,
+                name: 'Rolamento de roda',
                 price: "3200"
             },
             {
                 id: 9,
+                name: 'Barra de direção',
                 price: "1200"
             },
             {
                 id: 10,
+                name: 'Braços tensores',
                 price: "1870"
             }
         ];
@@ -114,26 +128,82 @@
         };
 
         vm.getPrice = function() {
-            if(vm.part_select) {
-                vm.message_price = 'Preço: ';
-                vm.part_price = vm.parts[vm.part_select - 1].price;
+            if(vm.part.part_select) {
+                vm.part.message_price = 'Preço: ';
+                vm.part.price = vm.parts_maintenance[vm.part.part_select - 1].price;
             } else {
-                vm.message_price = '';
-                vm.part_price = '';
+                vm.part.message_price = '';
+                vm.part.price = '';
             }
         };
 
         vm.updatePrice = function() {
-            if(vm.part_select && vm.qtd) {
-                vm.message_price = 'Preço: ';
-                vm.part_price = parseInt(vm.parts[vm.part_select - 1].price) * vm.qtd;
+            if(vm.part.part_select && vm.part.qtd) {
+                vm.part.message_price = 'Preço: ';
+                vm.part.price = parseInt(vm.parts_maintenance[vm.part.part_select - 1].price) * vm.part.qtd;
             }
+        };
+
+        vm.submitPart = function(part) {
+            vm.parts = $localstorage.getObject('parts');
+            var part_name = vm.parts_maintenance[vm.part.part_select - 1].name;
+
+            if(JSON.stringify(vm.parts) === '{}'){
+                $localstorage.setObject('parts', [{
+                    name: part_name,
+                    qtd: part.qtd,
+                    price: part.price
+                }]);
+            } else{
+                vm.parts.push({
+                    name: part_name,
+                    qtd: part.qtd,
+                    price: part.price
+                });
+                $localstorage.setObject('parts', vm.parts);
+            }
+
+            vm.parts = DataService.getPart();
+            vm.part = {};
+            jQuery(document).ready(function(){
+                jQuery("#myParts").modal("hide");
+
+                var bodyHeight = jQuery(this).height();
+                jQuery("html, body").animate({ scrollTop: 0 }, "slow");
+                jQuery(".content .clearfix").parent().animate({ height: (bodyHeight/2) }, "slow");
+            });
         };
 
         vm.submitAddMaintenance = function(form) {
             $localstorage.remove('maintenance');
             $localstorage.setObject('maintenance', form);
             $location.path('/add-maintenance-confirm');
+        };
+
+        jQuery(document).ready(function(){
+            jQuery('.popovers').popover();
+
+            jQuery('.default-date-picker').datepicker({
+                format: 'dd/mm/yyyy',
+                autoclose: true,
+                language: 'pt-BR'
+            });
+        });
+
+        vm.checkKm = function(km, qtd_km) {
+            if(km <= qtd_km) {
+                vm.view_status = true;
+            } else {
+                vm.view_status = false;
+            }
+        };
+
+        vm.checkStatus = function(status) {
+            if(status == 2) {
+                vm.view_date = true;
+            } else {
+                vm.view_date = false;
+            }
         };
 
     }
