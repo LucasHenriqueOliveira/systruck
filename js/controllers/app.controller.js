66c08384b550a05147cb5e27188a0791abc78674
Lucas <lucashen@gmail.com>
2016-02-12 09:31:41 -0200

    add controller app

---
+++ js/controllers/app.controller.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AppController', AppController);
+
+    AppController.$inject = ['$location'];
+
+    function AppController($location) {
+        var vm = this;
+
+        vm.logout = function() {
+
+        };
+    }
+
+})();
