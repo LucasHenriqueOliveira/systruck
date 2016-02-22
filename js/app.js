2fc0015ffd22498458c8b8781cd0dfeab019e2cf
Lucas <lucashen@gmail.com>
2016-02-22 08:17:55 -0300

    remove cookies and require the login in the pages

--- js/app.js
+++ js/app.js
@@ -2,223 +2,324 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngCookies', 'ui.utils.masks', 'ui.mask', 'ngAnimate', 'highcharts-ng'])
+        .module('app', ['ngRoute', 'ui.utils.masks', 'ui.mask', 'ngAnimate', 'highcharts-ng'])
         .config(config)
         .run(run);
 
-    config.$inject = ['$routeProvider'];
-    function config($routeProvider) {
+    config.$inject = ['$routeProvider', '$httpProvider'];
+    function config($routeProvider, $httpProvider) {
+        $httpProvider.interceptors.push('TokenInterceptor');
+
         $routeProvider
             .when('/', {
                 controller: 'DashboardController',
                 templateUrl: 'templates/dashboard.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/login', {
                 controller: 'LoginController',
                 templateUrl: 'templates/login.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: false
+                }
             })
 
             .when('/register', {
                 controller: 'RegisterController',
                 templateUrl: 'templates/register.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
+            })
+
+            .when('/select-profile', {
+                controller: 'SelectProfileController',
+                templateUrl: 'templates/select-profile.html',
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/profile', {
                 controller: 'ProfileController',
                 templateUrl: 'templates/profile.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/users', {
                 controller: 'UsersController',
                 templateUrl: 'templates/users.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/user/:id', {
                 controller: 'UserController',
                 templateUrl: 'templates/user.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/trucks', {
                 controller: 'TrucksController',
                 templateUrl: 'templates/trucks.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/truck/:id', {
                 controller: 'TruckController',
                 templateUrl: 'templates/truck.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/parts', {
                 controller: 'PartsController',
                 templateUrl: 'templates/parts.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/company', {
                 controller: 'CompanyController',
                 templateUrl: 'templates/company.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/edit-company', {
                 controller: 'EditCompanyController',
                 templateUrl: 'templates/edit-company.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/trip/:id', {
                 controller: 'TripController',
                 templateUrl: 'templates/trip.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/add-trip', {
                 controller: 'AddTripController',
                 templateUrl: 'templates/add-trip.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/add-trip-confirm', {
                 controller: 'AddTripConfirmController',
                 templateUrl: 'templates/add-trip-confirm.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/search-trip', {
                 controller: 'SearchTripController',
                 templateUrl: 'templates/search-trip.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/research-trip', {
                 controller: 'ResearchTripController',
                 templateUrl: 'templates/research-trip.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/last-trip', {
                 controller: 'LastTripController',
                 templateUrl: 'templates/last-trip.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/maintenance/:id', {
                 controller: 'MaintenanceController',
                 templateUrl: 'templates/maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/add-maintenance', {
                 controller: 'AddMaintenanceController',
                 templateUrl: 'templates/add-maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/add-maintenance-confirm', {
                 controller: 'AddMaintenanceConfirmController',
                 templateUrl: 'templates/add-maintenance-confirm.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/search-maintenance', {
                 controller: 'SearchMaintenanceController',
                 templateUrl: 'templates/search-maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/research-maintenance', {
                 controller: 'ResearchMaintenanceController',
                 templateUrl: 'templates/research-maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/last-maintenance', {
                 controller: 'LastMaintenanceController',
                 templateUrl: 'templates/last-maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/realized-maintenance', {
                 controller: 'RealizedMaintenanceController',
                 templateUrl: 'templates/realized-maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/all-periodics', {
                 controller: 'AllPeriodicsController',
                 templateUrl: 'templates/all-periodics.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-profit', {
                 controller: 'ReportProfitController',
                 templateUrl: 'templates/reports/profit.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-expense', {
                 controller: 'ReportExpenseController',
                 templateUrl: 'templates/reports/expense.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-maintenance', {
                 controller: 'ReportMaintenanceController',
                 templateUrl: 'templates/reports/maintenance.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-truck', {
                 controller: 'ReportTruckController',
                 templateUrl: 'templates/reports/truck.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-driver', {
                 controller: 'ReportDriverController',
                 templateUrl: 'templates/reports/driver.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-travel', {
                 controller: 'ReportTravelController',
                 templateUrl: 'templates/reports/travel.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .when('/report-part', {
                 controller: 'ReportPartController',
                 templateUrl: 'templates/reports/part.html',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                access: {
+                    requiredLogin: true
+                }
             })
 
             .otherwise({ redirectTo: '/login' });
     }
 
-    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
-    function run($rootScope, $location, $cookieStore, $http) {
-        // keep user logged in after page refresh
-        $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-        }
+    run.$inject = ['$rootScope', '$location', 'AuthenticationService'];
+    function run($rootScope, $location, AuthenticationService) {
 
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        $rootScope.$on('$locationChangeStart', function (event, nextRoute, currentRoute) {
             $rootScope.location = $location;
             // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $rootScope.login;
-            var loggedIn = $rootScope.globals.currentUser;
-            if (!restrictedPage && !loggedIn) {
-                $location.path('/login');
+
+            if ((nextRoute.access && nextRoute.access.requiredLogin) && !AuthenticationService.IsLogged) {
+                $location.path("/login");
             }
         });
     }
