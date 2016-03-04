e17ec19cf99f1b1eb7c4641d0a02779914b44d65
Lucas <lucashen@gmail.com>
2016-03-04 09:13:02 -0300

    send data to api

--- js/controllers/search.trip.controller.js
+++ js/controllers/search.trip.controller.js
@@ -5,18 +5,48 @@
         .module('app')
         .controller('SearchTripController', SearchTripController);
 
-    SearchTripController.$inject = ['$location'];
+    SearchTripController.$inject = ['$location', 'DataService', '$localstorage'];
 
-    function SearchTripController($location) {
+    function SearchTripController($location, DataService, $localstorage) {
         var vm = this;
+        vm.numberTrip = '';
+        vm.from = '';
+        vm.to = '';
+        vm.truckSelect = {};
+        vm.driverSelect = {};
+        vm.cityHome = {};
+        vm.cityDestination = {};
+        vm.message = '';
+        vm.results = {};
 
-        vm.submitSearchTrip = function(form){
+        DataService.getTrucksDriversCities().then(function (data) {
+            vm.trucks = data.getTrucks;
+            vm.drivers = data.getDrivers;
+            vm.cities = data.getCities;
+        });
 
-            if (typeof form.numberTrip !== "undefined"){
-                $location.path('/research-trip');
-            } else{
-                $location.path('/last-trip');
-            }
+        vm.submitSearchTrip = function(){
+            vm.message = '';
+
+            var postData = {
+                numberTrip: vm.numberTrip,
+                dateInitial: vm.from,
+                dateFinal: vm.to,
+                truckSelect: vm.truckSelect.id,
+                driverSelect: vm.driverSelect.id,
+                cityDestinationId: vm.cityDestination.id,
+                cityHomeId: vm.cityHome.id,
+                company: $localstorage.getObject('company')
+            };
+
+            DataService.getSearchTrip(postData).then(function(response) {
+
+                if(response.error === false) {
+                    vm.results = response.trip;
+                } else {
+                    vm.message = response.message;
+                }
+            });
         };
 
         jQuery(document).ready(function(){
