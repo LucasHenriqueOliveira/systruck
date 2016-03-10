e7aa03fef9db8d476133831905c93afb2534276d
Lucas <lucashen@gmail.com>
2016-03-10 10:39:08 -0300

    add function submit edit company

--- js/controllers/edit.company.controller.js
+++ js/controllers/edit.company.controller.js
@@ -5,9 +5,9 @@
         .module('app')
         .controller('EditCompanyController', EditCompanyController);
 
-    EditCompanyController.$inject = ['$window', 'DataService'];
+    EditCompanyController.$inject = ['$window', 'DataService', '$location'];
 
-    function EditCompanyController($window, DataService) {
+    function EditCompanyController($window, DataService, $location) {
         var vm = this;
         vm.cities = {};
         vm.company = DataService.getCompanyLocal();
@@ -22,6 +22,29 @@
 
         vm.back = function(){
             $window.history.back();
+        };
+
+        vm.editCompany = function(data) {
+            var dataCompany = {
+                id: data.company.empresa_id,
+                logradouro: data.company.empresa_logradouro,
+                numero: data.company.empresa_numero,
+                complemento: data.company.empresa_complemento,
+                bairro: data.company.empresa_bairro,
+                cidade_id: data.cidade_id.id,
+                cep: data.company.empresa_cep,
+                telefone1: data.company.empresa_telefone_1,
+                telefone2: data.company.empresa_telefone_2
+            };
+
+            DataService.editCompany(dataCompany).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Empresa', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Empresa', {timeOut: 3000});
+                    $location.path('/company');
+                }
+            });
         };
     }
 
