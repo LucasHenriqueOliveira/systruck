df31d077a4387f3540fa313f531a5e3a19a806b7
Lucas <lucashen@gmail.com>
2016-03-18 11:20:25 -0300

    add functions create, active, remove, edit

--- js/controllers/users.controller.js
+++ js/controllers/users.controller.js
@@ -5,9 +5,9 @@
         .module('app')
         .controller('UsersController', UsersController);
 
-    UsersController.$inject = ['$location'];
+    UsersController.$inject = ['$location', 'UserService', '$localstorage'];
 
-    function UsersController($location) {
+    function UsersController($location, UserService, $localstorage) {
         var vm = this;
         vm.show_driver = false;
 
@@ -17,6 +17,82 @@
             } else {
                 vm.show_driver = false;
             }
+        };
+
+        vm.getUsers = function() {
+            UserService.getUsers().then(function (data) {
+                vm.usersAvailable = data.getUsersAvailable;
+                vm.usersUnavailable = data.getUsersUnavailable;
+            });
+        };
+
+        vm.getUsers();
+
+        vm.getUser = function(user) {
+            UserService.setCurrentUser(user);
+            $location.path('/user');
+        };
+
+        vm.editUser = function(user) {
+            UserService.setCurrentUser(user);
+            $location.path('/edit-user');
+        };
+
+        vm.removeUser = function(user) {
+            var userRemove = {
+                id: $localstorage.getObject('id')
+            };
+            UserService.removeUser(user.perfil_id, userRemove).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Usuário', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Usuário', {timeOut: 3000});
+                }
+                vm.getUsers();
+            });
+        };
+
+        vm.activeUser = function(user) {
+            var userActive = {
+                id: $localstorage.getObject('id')
+            };
+            UserService.activeUser(user.perfil_id, userActive).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Usuário', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Usuário', {timeOut: 3000});
+                }
+                vm.getUsers();
+            });
+        };
+
+        vm.submitUser = function(form) {
+            var dataUser = {
+                nome: form.user.name,
+                tipo_perfil: form.user.function,
+                email: form.user.email,
+                cnh: form.user.cnh,
+                data_exame: form.user.date_periodic,
+                telefone1: form.user.phone1,
+                telefone2: form.user.phone2,
+                usuario_ativacao: $localstorage.getObject('id'),
+                empresa: $localstorage.getObject('company')
+            };
+
+            UserService.create(dataUser).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Usuário', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Usuário', {timeOut: 3000});
+                    vm.user = {};
+
+                    jQuery(document).ready(function(){
+                        jQuery("#myModal").modal("hide");
+                    });
+
+                    vm.getUsers();
+                }
+            });
         };
 
         jQuery(document).ready(function(){
