ad842ff14d5c42c60de0c9584f2c59af364d0a58
Lucas <lucashen@gmail.com>
2016-03-28 10:07:47 -0300

    put the functions

--- js/controllers/trucks.controller.js
+++ js/controllers/trucks.controller.js
@@ -5,10 +5,57 @@
         .module('app')
         .controller('TrucksController', TrucksController);
 
-    TrucksController.$inject = ['$location'];
+    TrucksController.$inject = ['$location', 'TruckService', '$localstorage'];
 
-    function TrucksController($location) {
+    function TrucksController($location, TruckService, $localstorage) {
         var vm = this;
+
+        vm.getTrucks = function() {
+            TruckService.getTrucks().then(function (data) {
+                vm.trucksAvailable = data.getTrucksAvailable;
+                vm.trucksUnavailable = data.getTrucksUnavailable;
+            });
+        };
+
+        vm.getTrucks();
+
+        vm.getTruck = function(truck) {
+            TruckService.setCurrentTruck(truck);
+            $location.path('/truck/' + truck.carro_id);
+        };
+
+        vm.editTruck = function(truck) {
+            TruckService.setCurrentTruck(truck);
+            $location.path('/edit-truck/' + truck.carro_id);
+        };
+
+        vm.removeTruck = function(truck) {
+            var truckRemove = {
+                id: $localstorage.getObject('id')
+            };
+            TruckService.removeTruck(truck.carro_id, truckRemove).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Caminhão', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Caminhão', {timeOut: 3000});
+                }
+                vm.getTrucks();
+            });
+        };
+
+        vm.activeTruck = function(truck) {
+            var truckActive = {
+                id: $localstorage.getObject('id')
+            };
+            TruckService.activeTruck(truck.carro_id, truckActive).then(function (data) {
+                if(data.error) {
+                    toastr.error(data.message, 'Caminhão', {timeOut: 3000});
+                } else {
+                    toastr.success(data.message, 'Caminhão', {timeOut: 3000});
+                }
+                vm.getTrucks();
+            });
+        };
 
     }
 
