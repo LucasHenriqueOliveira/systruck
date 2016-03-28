ee2cf26b8dc47596fd09e5acec5dcf16725b8569
Lucas <lucashen@gmail.com>
2016-03-28 10:07:13 -0300

    get data in api

--- js/controllers/truck.controller.js
+++ js/controllers/truck.controller.js
@@ -5,18 +5,35 @@
         .module('app')
         .controller('TruckController', TruckController);
 
-    TruckController.$inject = ['$location', '$window', '$timeout'];
+    TruckController.$inject = ['$location', '$window', 'TruckService', '$routeParams'];
 
-    function TruckController($location, $window, $timeout) {
+    function TruckController($location, $window, TruckService, $routeParams) {
         var vm = this;
         vm.loading = true;
-
-        $timeout(function() {
-            vm.loading = false;
-        }, 2000);
+        vm.truck = {};
 
         vm.back = function(){
             $window.history.back();
+        };
+
+        vm.truck = TruckService.getCurrentTruck();
+
+        if(!vm.truck) {
+            TruckService.getById($routeParams.id).then(function (data) {
+                vm.truck = data.getTruck;
+                vm.truckPart = data.getTruckPart;
+                vm.loading = false;
+            });
+        } else {
+            TruckService.getTruckPart(vm.truck.carro_id).then(function (data) {
+                vm.truckPart = data.getTruckPart;
+                vm.loading = false;
+            });
+        }
+
+        vm.editTruck = function(truck) {
+            TruckService.setCurrentTruck(truck);
+            $location.path('/edit-truck/' + truck.carro_id);
         };
     }
 
