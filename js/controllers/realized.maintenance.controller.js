7497da2b8150e2cca1cf2ab7dfef53e4ca60608a
Lucas <lucashen@gmail.com>
2016-03-31 23:24:46 -0300

    get data to api

--- js/controllers/realized.maintenance.controller.js
+++ js/controllers/realized.maintenance.controller.js
@@ -5,11 +5,28 @@
         .module('app')
         .controller('RealizedMaintenanceController', RealizedMaintenanceController);
 
-    RealizedMaintenanceController.$inject = ['$location'];
+    RealizedMaintenanceController.$inject = ['$location', 'DataService', 'PartService'];
 
-    function RealizedMaintenanceController($location) {
+    function RealizedMaintenanceController($location, DataService, PartService) {
         var vm = this;
 
+        vm.getRealizedMaintenance = function() {
+            DataService.getRealizedMaintenance().then(function (data) {
+                vm.maintenances = data.getRealizedMaintenance;
+            });
+        };
+
+        vm.getRealizedMaintenance();
+
+        vm.getMaintenance = function(maintenance) {
+            PartService.setCurrentPart(maintenance);
+            $location.path('/maintenance/' + maintenance.revisao_id);
+        };
+
+        vm.editMaintenance = function(maintenance) {
+            PartService.setCurrentPart(maintenance);
+            $location.path('/edit-maintenance/' + maintenance.revisao_id);
+        };
     }
 
 })();
